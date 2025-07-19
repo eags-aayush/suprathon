7155fa6dd7de4382381d0b4626950f369fe4b024
Aayush Laddha <aayushladdha001@gmail.com>
2025-07-19 19:08:44 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     `nav-link hover:font-bold transition-all duration-200 ease-in-out ${isActive ? 'font-bold border-b-2 border-text' : ''}`
 
   return (
-    <nav className='z-50 sticky top-10 px-5 py-3 font-light w-3/4 m-auto backdrop-blur-md bg-white/10 border-2 border-text rounded-full'>
+    <nav className='z-50 sticky top-10 px-5 py-3 font-light w-3/4 m-auto backdrop-blur-md bg-white/10 border-2 border-text rounded-3xl md:rounded-full'>
       <ul className='hidden md:flex justify-between gap-4 items-center'>
         <span className='flex gap-3 items-center'>
           <img src={Logo} alt="logo" className='w-7 h-7 rounded-full'/>
@@ -56,7 +56,7 @@ const Navbar = () => {
       </ul>
 
       {isOpen && (
-        <div className=' absolute top-10 right-0 bg-background text-text p-4 rounded-md'>
+        <div className='top-17 right-0 text-text p-4 rounded-md'>
           <ul className='md:hidden flex flex-col gap-4'>
             <NavLink to="/" onClick={() => setIsOpen(!isOpen)} className={linkClass}>Home</NavLink>
             <NavLink to="/analyze" onClick={() => setIsOpen(!isOpen)} className={linkClass}>Analyze</NavLink>
